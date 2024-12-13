8da9177739778d0e67518d468c4a9f134c30c7c3
Selva Jo <appslankasoftwaresolutions@gmail.com>
2024-12-13 15:52:22 +0100

    added jobs

---
+++ src/Filament/Resources/FailedJobResource/Pages/ManageFailedJobs.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Appslanka\JobWatcher\Resources\FailedJobResource\Pages;
+
+use Appslanka\JobWatcher\Resources\FailedJobResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageFailedJobs extends ManageRecords
+{
+    protected static string $resource = FailedJobResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
